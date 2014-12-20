b2ed9d85673bbd78a3e968c606bfaf9756b77615
Craig Webster <craig.webster@mac.com>
2014-12-20 21:43:59 +0000

    no message

---
+++ Pods/Target Support Files/Pods-NLMCTests-MagicalRecord/Pods-NLMCTests-MagicalRecord-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NLMCTests_MagicalRecord : NSObject
+@end
+@implementation PodsDummy_Pods_NLMCTests_MagicalRecord
+@end
---
+++ Pods/Target Support Files/Pods-NLMCTests/Pods-NLMCTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NLMCTests : NSObject
+@end
+@implementation PodsDummy_Pods_NLMCTests
+@end
