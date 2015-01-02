041c2a61cbb70a2fd85e464d8980df32f42cab09
Craig Webster <craig.webster@mac.com>
2015-01-02 22:02:14 +0000

    Update

---
+++ Pods/Target Support Files/Pods-NLMC-TBXML/Pods-NLMC-TBXML-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NLMC_TBXML : NSObject
+@end
+@implementation PodsDummy_Pods_NLMC_TBXML
+@end
---
+++ Pods/Target Support Files/Pods-NLMCTests-TBXML/Pods-NLMCTests-TBXML-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NLMCTests_TBXML : NSObject
+@end
+@implementation PodsDummy_Pods_NLMCTests_TBXML
+@end
